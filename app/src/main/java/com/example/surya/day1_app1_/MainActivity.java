24eb093f0edf99466b986efe0df0401edb3e3931
keithgeorge <keithgeorge095@gmail.com>
2016-10-11 23:30:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/surya/day1_app1_/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.surya.day1_app1_;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by surya on 11/10/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+}
