a19ee1df8a80dec63a8d49dbafd0f4f031ff5ea4
keithgeorge <keithgeorge095@gmail.com>
2016-10-13 09:10:18 +0530

    second commit

--- app/src/main/java/com/example/surya/day1_app1_/MainActivity.java
+++ app/src/main/java/com/example/surya/day1_app1_/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.surya.day1_app1_;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by surya on 11/10/16.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ctivity_main);
+
+        TextView textView=(TextView)findViewById(R.id.textView1);
+        Toast.makeText(this,"First Activity",Toast.LENGTH_LONG).show();
+        
+    }
 }
---
+++ app/src/main/java/com/example/surya/day1_app1_/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.surya.day1_app1_;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by surya on 13/10/16.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+
+    }
+
+
+}
